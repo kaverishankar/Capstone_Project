e18de8e8b9d3d49ac5a3d3d9af5f8603203c90ff
Kaveri shankar M <71606190+kaverishankar@users.noreply.github.com>
2024-09-30 15:15:45 +0530

    Update productInfo.jsx

--- src/pages/productInfo.jsx
+++ src/pages/productInfo.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Product from '../components/Product.jsx';
+import Product from '../components/product.jsx';
 import { handledAPIGet } from "../apis/auth.js";
 import Loader from "../components/Loader.jsx";
 
