c2d5f26d6c3c4379550406bd0e1cd933252f31b5
Kaveri shankar M <71606190+kaverishankar@users.noreply.github.com>
2024-10-04 20:18:42 +0530

    Update api-instance.js

--- src/store/api-instance.js
+++ src/store/api-instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://real-estate-backend-project-dhey.onrender.com",
   timeout: 10000,
   headers: {
     "Customer-Header": "Hi Heloo",
